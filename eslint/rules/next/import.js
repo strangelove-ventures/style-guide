cea1aebc18c22a94314852a56b2d5a0c5892fe9d
Griko Nibras <git@griko.id>
2022-10-06 09:40:47 +0700

    feat(eslint): disable unicorn/filename-case for next pages

--- eslint/rules/next/import.js
+++ eslint/rules/next/import.js
@@ -7,6 +7,7 @@ const config = {
       files: ["pages/**/*.{js,jsx,ts,tsx}", "next.config.{js,cjs,mjs}"],
       rules: {
         "import/prefer-default-export": ["error"],
+        "unicorn/filename-case": ["off"],
       },
     },
   ],
