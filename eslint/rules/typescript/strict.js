b6c009a5a9ec7a6eb431fa44f7608b465280ea8d
Griko Nibras <griko@nibras.co>
2023-03-11 01:08:40 +0700

    feat: update eslint ts rules

--- eslint/rules/typescript/strict.js
+++ eslint/rules/typescript/strict.js
@@ -4,6 +4,9 @@
 const config = {
   rules: {
     "@typescript-eslint/explicit-function-return-type": ["off"],
+    "@typescript-eslint/no-misused-promises": ["warn"],
+    "@typescript-eslint/no-unsafe-assignment": ["warn"],
+    "@typescript-eslint/no-unsafe-member-access": ["warn"],
     "@typescript-eslint/prefer-nullish-coalescing": ["off"],
     "@typescript-eslint/restrict-template-expressions": ["off"],
   },
