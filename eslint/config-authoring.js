1c2ec2488aeff68d46f7bc728093d49909c6366d
Griko Nibras <griko@nibras.co>
2023-04-12 01:23:58 +0700

    feat: update eslint config

--- eslint/config-authoring.js
+++ eslint/config-authoring.js
@@ -3,6 +3,10 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
   extends: require.resolve("./node"),
+  rules: {
+    "eslint-comments/no-unlimited-disable": ["off"],
+    "unicorn/prefer-node-protocol": ["off"],
+  },
   overrides: [
     {
       files: ["eslint/rules/**", "prettier/**"],
