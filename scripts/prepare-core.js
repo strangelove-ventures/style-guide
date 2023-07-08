3d8284dbb78b613087b0f776976dce491dadb404
Griko Nibras <griko@nibras.co>
2023-07-09 00:35:26 +0700

    feat: update prepare-core script

--- scripts/prepare-core.js
+++ scripts/prepare-core.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require("fs/promises");
+const spawn = require("child_process").spawnSync;
 
 const packageJson = require("../package.json");
 
@@ -9,8 +10,6 @@ const prepareCore = async () => {
 
   const depsToDelete = {
     dependencies: ["eslint-plugin-jsx-a11y", "eslint-plugin-react", "eslint-plugin-react-hooks"],
-    peerDependencies: ["tailwindcss"],
-    peerDependenciesMeta: ["tailwindcss"],
     optionalDependencies: ["@next/eslint-plugin-next", "eslint-plugin-tailwindcss"],
   };
 
@@ -20,10 +19,21 @@ const prepareCore = async () => {
     }
   }
 
-  await fs.writeFile("./package.json", `${JSON.stringify(packageJson, null, 2)}\n`);
+  await fs.writeFile("package.json", `${JSON.stringify(packageJson, null, 2)}\n`);
+  await fs.copyFile("README.core.md", "README.md");
 
-  await fs.copyFile("./README.core.md", "./README.md");
-  await fs.unlink("./README.core.md");
+  const filesToDelete = [
+    "eslint/rules/jsx-a11y.js",
+    "eslint/rules/react.js",
+    "eslint/next.js",
+    "eslint/react.js",
+    "eslint/tailwindcss.js",
+    "README.core.md",
+  ];
+
+  await Promise.all(filesToDelete.map((file) => fs.unlink(file))).catch(() => {});
+
+  spawn("pnpm", ["install"], { shell: true });
 };
 
 void prepareCore();
